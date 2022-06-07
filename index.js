ccd2c5cee995dc4c091c3b2c0d334b677bde48d3
matkaingo <mateogomezrios17@gmail.com>
2022-06-07 10:06:57 -0500

    change JS name

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get("/", function(req, res) {
-    res.send("It Worked");
-})
-
-app.listen(process.env.PORT || 5000)
-module.exports = app;
